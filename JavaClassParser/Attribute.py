26b566deb19d8d7cf5c7940e4671ba1c6f0023a8
mkucijan <mkucijan@gmail.com>
2017-12-13 12:40:54 +0100

    Initial Commit

---
+++ JavaClassParser/Attribute.py
@@ -0,0 +1,419 @@
+__author__ = 'jason'
+
+from JavaClassParser import ByteToHex, ByteToDec
+from JavaClassParser.ExceptionInfo import ExceptionInfo
+from JavaClassParser import InnerClassAccessFlags
+
+CODE_NAME = 'Code'
+LINE_NUMBER_TABLE_NAME = 'LineNumberTable'
+LOCAL_VARIABLE_TABLE_NAME = "LocalVariableTable"
+SOURCE_FILE_NAME = 'SourceFile'
+CONSTANT_VALUE_NAME = 'ConstantValue'
+INNER_CLASSES_NAME = 'InnerClasses'
+DEPRECATED_NAME = 'Deprecated'
+SYNTHETIC_NAME = 'Synthetic'
+
+class Attribute(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+    @staticmethod
+    def getParser(attrName):
+        attrName=attrName.decode('utf-8')
+        parserDict = {
+            CODE_NAME: CodeAttribute.parse,
+            LINE_NUMBER_TABLE_NAME: LineNumberTableAttribute.parse,
+            LOCAL_VARIABLE_TABLE_NAME: LocalVariableTableAttribute.parse,
+            SOURCE_FILE_NAME: SourceFileAttribute.parse,
+            SYNTHETIC_NAME: SyntheticAttribute.parse,
+            DEPRECATED_NAME: DeprecatedAttribute.parse,
+            INNER_CLASSES_NAME: InnerClassesAttribute.parse,
+            CONSTANT_VALUE_NAME: ConstantValueAttribute.parse,
+        }
+
+        if attrName in parserDict:
+            return parserDict[str(attrName)]
+        else:
+            return None
+
+class ConstantValueAttribute(Attribute):
+
+    def __init__(self):
+        super(ConstantValueAttribute, self).__init__(CONSTANT_VALUE_NAME)
+
+        self.constantValueIndex = -1
+
+    def setValueIndex(self, index):
+
+        self.constantValueIndex = index
+
+    def getValueIndex(self):
+
+        return self.constantValueIndex
+
+    def __str__(self):
+
+        return "Constant Value: #%d" % self.constantValueIndex
+
+    @staticmethod
+    def parse(clsFile, result):
+
+        attribute = ConstantValueAttribute()
+
+        classIndex = ByteToDec(clsFile.read(2))
+
+        attribute.setValueIndex(classIndex)
+
+        return attribute
+
+class SyntheticAttribute(Attribute):
+
+    def __init__(self):
+        super(SyntheticAttribute, self).__init__(SYNTHETIC_NAME)
+
+    def __str__(self):
+        return "Synthetic"
+
+    @staticmethod
+    def parse(clsFile, result):
+
+        attribute = SyntheticAttribute()
+
+        return attribute
+
+class DeprecatedAttribute(Attribute):
+
+    def __init__(self):
+        super(DeprecatedAttribute, self).__init__(DEPRECATED_NAME)
+
+    def __str__(self):
+        return "Deprecated"
+
+    @staticmethod
+    def parse(clsFile, result):
+
+        attribute = DeprecatedAttribute()
+
+        return attribute
+
+class InnerClassesAttribute(Attribute):
+
+    class InnerClassesInfo:
+
+        def __init__(self, innerClassIndex, outerClassIndex, innerNameIndex, innerClassAccessFlags):
+            self.innerClassIndex = innerClassIndex
+            self.outerClassIndex = outerClassIndex
+            self.innerNameIndex = innerNameIndex
+            self.innerClassAccessFlags = innerClassAccessFlags
+
+        def __str__(self):
+
+            result = ''
+
+            result += '#%d(Inner class #%d, Outer class #%d, Access %s)' % (self.innerNameIndex,
+                                                                                    self.innerClassIndex,
+                                                                                    self.outerClassIndex, InnerClassAccessFlags.flagToStr(self.innerClassAccessFlags))
+            return result
+
+    def __init__(self):
+
+        super(InnerClassesAttribute, self).__init__(INNER_CLASSES_NAME)
+
+        self.classes = []
+
+    def addInnerClass(self, innerClass):
+
+        self.classes.append(innerClass)
+
+    def __str__(self):
+
+        result = ""
+
+        result += "InnerClasses\n"
+        result += "==============================\n"
+
+        for innerClass in self.classes:
+
+            result += str(innerClass) + "\n"
+
+        result += "\n"
+
+        return result
+
+
+    @staticmethod
+    def parse(clsFile, result):
+
+        attribute = InnerClassesAttribute()
+
+        numberOfClasses = ByteToDec(clsFile.read(2))
+
+        if numberOfClasses > 0:
+
+            for i in range(numberOfClasses):
+
+                innerClassIndex = ByteToDec(clsFile.read(2))
+                outerClassIndex = ByteToDec(clsFile.read(2))
+                innerNameIndex = ByteToDec(clsFile.read(2))
+                innerClassAccessFlags = ByteToDec(clsFile.read(2))
+
+                innerClassInfo = InnerClassesAttribute.InnerClassesInfo(innerClassIndex, outerClassIndex, innerNameIndex, innerClassAccessFlags)
+
+                attribute.addInnerClass(innerClassInfo)
+
+
+        return attribute
+
+class SourceFileAttribute(Attribute):
+
+    def __init__(self):
+
+        super(SourceFileAttribute, self).__init__(SOURCE_FILE_NAME)
+
+        self.sourceFileName = ''
+
+    def getSourceFile(self):
+        return self.sourceFileName
+
+    def setSourceFile(self, sourceFileName):
+        self.sourceFileName = sourceFileName
+
+    def __str__(self):
+
+        return 'Source File: %s' % self.sourceFileName
+
+    @staticmethod
+    def parse(clsFile, result):
+
+        attribute = SourceFileAttribute()
+
+        sourceFileIndex = ByteToDec(clsFile.read(2))
+
+        attribute.setSourceFile(result.getUtf8(sourceFileIndex))
+
+        return attribute
+
+class LocalVariableTableAttribute(Attribute):
+
+    class LocalVarInfo(object):
+
+        def __init__(self, startPC, length, name, descriptor, index):
+            self.startPC = startPC
+            self.length = length
+            self.name = name
+            self.descriptor = descriptor
+            self.index = index
+
+        def __str__(self):
+            return "Var: %s(%s) startPC: %d length: %d index: %d" % (self.name, self.descriptor, self.startPC, self.length, self.index)
+
+    def __init__(self):
+        super(LocalVariableTableAttribute, self).__init__(LOCAL_VARIABLE_TABLE_NAME)
+        self.varInfos = []
+
+    def addVarInfo(self, varInfo):
+        self.varInfos.append(varInfo)
+
+    def __str__(self):
+
+        result = ""
+
+        result += "Local Variable Table\n"
+        result += "===============================\n"
+
+        for varInfo in self.varInfos:
+            result += str(varInfo) + "\n"
+
+        result += "\n"
+
+        return result
+
+    @staticmethod
+    def parse(clsFile, result):
+
+        attribute = LocalVariableTableAttribute()
+
+        variableCount = ByteToDec(clsFile.read(2))
+
+        for i in range(variableCount):
+            startPC = ByteToDec(clsFile.read(2))
+            length = ByteToDec(clsFile.read(2))
+            argName = result.getUtf8(ByteToDec(clsFile.read(2)))
+            descriptorName = result.getUtf8(ByteToDec(clsFile.read(2)))
+            index = ByteToDec(clsFile.read(2))
+
+            info = LocalVariableTableAttribute.LocalVarInfo(startPC, length, argName, descriptorName, index)
+
+            attribute.addVarInfo(info)
+
+        return attribute
+
+
+class LineNumberTableAttribute(Attribute):
+    def __init__(self):
+        super(LineNumberTableAttribute, self).__init__(LINE_NUMBER_TABLE_NAME)
+
+        # a dict {start_pc, line_number}
+        self.code2Line = {}
+
+    def getCode2Line(self):
+
+        return self.code2Line
+
+    def addCode2Line(self, startPC, lineNumber):
+        self.code2Line[startPC] = lineNumber
+
+    def __str__(self):
+        result = ""
+        result += "LineNumberTable\n"
+        result += "======================\n"
+
+        for aKey in self.code2Line.keys():
+            result += "%5d -> %5d\n" % (aKey, self.code2Line[aKey])
+
+        result += "\n"
+
+        return result
+
+    @staticmethod
+    def parse(clsFile, result):
+
+        attribute = LineNumberTableAttribute()
+
+        tableLen = ByteToDec(clsFile.read(2))
+
+        for i in range(tableLen):
+            startPC = ByteToDec(clsFile.read(2))
+            lineNumber = ByteToDec(clsFile.read(2))
+
+            attribute.addCode2Line(startPC, lineNumber)
+
+        return attribute
+
+
+import JavaClassParser.ByteCode as bc
+
+class CodeAttribute(Attribute):
+
+    def __init__(self):
+        super(CodeAttribute, self).__init__(CODE_NAME)
+
+        self.maxStack = -1
+        self.maxLocal = -1
+
+        self.exceptions = []
+
+        self.code = None
+        self.instructions = None
+
+        self.attributes = []
+
+    @staticmethod
+    def getAttrName():
+        return CODE_NAME
+
+    def setMaxStack(self, maxStack):
+        self.maxStack = maxStack
+
+    def setMaxLocal(self, maxLocal):
+        self.maxLocal = maxLocal
+
+    def setCode(self, code):
+        self.code = code
+
+    def addException(self, exception):
+        self.exceptions.append(exception)
+
+    def addAttribute(self, attribute):
+        if attribute.name is LINE_NUMBER_TABLE_NAME:
+            self.lineNumberTable = attribute
+        self.attributes.append(attribute)
+
+    def addInstructions(self):
+        self.instructions = []
+        byteIndex = 0
+        while byteIndex < len(self.code):
+            byteCode = bc.codeMap[int(self.code[byteIndex:byteIndex+2], 16)]
+
+            if byteCode:
+                self.instructions.append(byteCode(self.code,byteIndex))
+                byteIndex += byteCode.getOpCodeCount() * 2
+
+            byteIndex += 2
+
+    def __str__(self):
+
+        result = ""
+
+        result += "\tCodeAttr\n"
+        result += "\t\tMax Stack: %d Max Local: %d\n" % (self.maxStack, self.maxLocal)
+        result += "\t\t-----------------------------------------------------------------\n"
+        result += "\t\tCode: " + self.code + "\n"
+        
+        for instruction in self.instructions:
+            result += str(instruction)
+
+        if len(self.exceptions) > 0:
+
+            result += "\t\tExceptions\n"
+            result += "\t\t================\n"
+            result += "\t\tStart\tEnd\tHandler\tClass\n"
+
+            for i in range(len(self.exceptions)):
+                result += "\t\t" + str(self.exceptions[i]) + "\n"
+
+        for subAttr in self.attributes:
+            result += "\t\t" + str(subAttr)
+            result += "\n"
+
+        return result
+
+    @staticmethod
+    def parse(clsFile, result):
+        codeAttr = CodeAttribute()
+
+        maxStack = ByteToDec(clsFile.read(2))
+        maxLocal = ByteToDec(clsFile.read(2))
+
+        codeLength = ByteToDec(clsFile.read(4))
+        code = ByteToHex(clsFile.read(codeLength))
+
+        codeAttr.setMaxStack(maxStack)
+        codeAttr.setMaxLocal(maxLocal)
+        codeAttr.setCode(code)
+
+        exceptionLen = ByteToDec(clsFile.read(2))
+        if exceptionLen > 0:
+            for i in range(exceptionLen):
+                startPC = ByteToDec(clsFile.read(2))
+                endPC = ByteToDec(clsFile.read(2))
+                handlerPC = ByteToDec(clsFile.read(2))
+                classIndex = ByteToDec(clsFile.read(2))
+
+                exceptionClassName = "any"
+                if classIndex > 0: # if class index is 0, the class name is any.
+                    exceptionClassName = result.getClassName(classIndex)
+
+                exceptionInfo = ExceptionInfo(startPC, endPC, handlerPC, exceptionClassName)
+
+                codeAttr.addException(exceptionInfo)
+
+        subAttrCount = ByteToDec(clsFile.read(2))
+        if subAttrCount > 0:
+            for i in range(subAttrCount):
+                subAttr = result.getUtf8(ByteToDec(clsFile.read(2)))
+                subAttrLength = ByteToDec(clsFile.read(4))
+
+                parser = Attribute.getParser(subAttr)
+                if parser is not None:
+                    attribute = parser(clsFile, result)
+                    codeAttr.addAttribute(attribute)
+                else:
+                    clsFile.read(subAttrLength)
+
+        codeAttr.addInstructions()
+        return codeAttr
---
+++ JavaClassParser/ConstantPool.py
@@ -0,0 +1,427 @@
+__author__ = 'jasonlee'
+
+from JavaClassParser.ParsingException import ParsingException
+from JavaClassParser import ByteToDec,ByteToHex, ByteTo32BitFloat,ByteTo64BitFloat
+
+CONSTANT_UTF8_INFO = 0x01
+CONSTANT_INTEGER_INFO = 0x03
+CONSTANT_FLOAT_INFO = 0x04
+CONSTANT_LONG_INFO = 0x05
+CONSTANT_DOUBLE_INFO = 0x06
+CONSTANT_CLASS_INFO = 0x07
+CONSTANT_STRING_INFO = 0x08
+CONSTANT_FIELDREF_INFO = 0x09
+CONSTANT_METHODREF_INFO = 0x0A
+CONSTANT_INTERFACEMETHODREF_INFO = 0x0B
+CONSTANT_NAMEANDTYPE_INFO = 0x0C
+
+TYPE_UTF8 = "UTF8"
+
+class ConstantInfo(object):
+    """
+    Base class of constant info.
+    """
+
+    def __init__(self, typeTag):
+        self.type = typeTag
+
+    def getTypeTag(self):
+        return self.type
+
+    def getTypeName(self):
+        raise ParsingException("Not implement this function in ConstantInfo")
+
+class ConstantClassInfo(ConstantInfo):
+    """
+    Constant Class Info
+    """
+
+    def __init__(self, nameIndex):
+        super(ConstantClassInfo, self).__init__(CONSTANT_CLASS_INFO)
+        self.nameIndex = nameIndex
+
+    def setNameIndex(self, nameIndex):
+        self.nameIndex = nameIndex
+
+    def getNameIndex(self):
+        return self.nameIndex
+
+    def getTypeName(self):
+        return "ClassInfo"
+
+    def __str__(self):
+        return "%s: #%d" % (self.getTypeName(), self.getNameIndex())
+
+    @staticmethod
+    def parse(fh): # fh stands for file handler
+        """
+        Constant class section has 3 bytes.
+        """
+
+        assert fh is not None
+
+        nameIndex = ByteToDec(fh.read(2))
+
+        return ConstantClassInfo(nameIndex)
+
+class ConstantUTF8Info(ConstantInfo):
+    """
+    Constant UTF8 Info
+    """
+
+    def __init__(self, length, utf8):
+        super(ConstantUTF8Info, self).__init__(CONSTANT_UTF8_INFO)
+        self.length = length
+        self.utf8 = utf8
+
+    def getTypeName(self):
+        return TYPE_UTF8
+
+    def __str__(self):
+        return "%s: %s (%d chars)" % (self.getTypeName(), self.utf8, self.length)
+
+    def getUtf8(self):
+        return self.utf8
+
+    @staticmethod
+    def parse(fh):
+        """
+        Constant UTF8 section
+        """
+
+        assert fh is not None
+
+        length = ByteToDec(fh.read(2))
+        content = fh.read(length)
+
+        return ConstantUTF8Info(length, content)
+
+class ConstantIntegerInfo(ConstantInfo):
+
+    def __init__(self, integer):
+
+        super(ConstantIntegerInfo, self).__init__(CONSTANT_INTEGER_INFO)
+
+        self.integer = integer
+
+    def getInteger(self):
+
+        return self.integer
+
+    def __str__(self):
+
+        return "Integer: %d" % self.integer
+
+    @staticmethod
+    def parse(fh):
+
+        assert fh is not None
+
+        return ConstantIntegerInfo(ByteToDec(fh.read(4)))
+
+class ConstantPlaceHolder(ConstantInfo):
+
+    def __init__(self):
+        super(ConstantPlaceHolder, self).__init__("PlaceHolder")
+
+    def __str__(self):
+        return "PlaceHolder"
+
+class ConstantLongInfo(ConstantInfo):
+
+    def __init__(self, long):
+
+        super(ConstantLongInfo, self).__init__(CONSTANT_LONG_INFO)
+
+        self.long = long
+
+    def getLong(self):
+
+        return self.long
+
+    def __str__(self):
+
+        return "Long: %d" % self.long
+
+    @staticmethod
+    def parse(fh):
+
+        assert fh is not None
+
+        return ConstantLongInfo(ByteToDec(fh.read(8)))
+
+class ConstantFloatInfo(ConstantInfo):
+
+    def __init__(self, floatNum):
+
+        super(ConstantFloatInfo, self).__init__(CONSTANT_FLOAT_INFO)
+
+        self.floatNum = floatNum
+
+    def getFloat(self):
+
+        return self.floatNum
+
+    def __str__(self):
+
+        return "Float: %s" % ByteTo32BitFloat(self.floatNum)
+
+    @staticmethod
+    def parse(fh):
+
+        assert fh is not None
+
+        return ConstantFloatInfo(fh.read(4))
+
+class ConstantDoubleInfo(ConstantInfo):
+
+    def __init__(self, doubleNum):
+
+        super(ConstantDoubleInfo, self).__init__(CONSTANT_DOUBLE_INFO)
+
+        self.doubleNum = doubleNum
+
+    def getDouble(self):
+
+        return self.doubleNum
+
+    def __str__(self):
+
+        return "Double: %s" % ByteTo64BitFloat(self.doubleNum)
+
+    @staticmethod
+    def parse(fh):
+
+        assert fh is not None
+
+        return ConstantDoubleInfo(fh.read(8))
+
+class ConstantStringInfo(ConstantInfo):
+
+    def __init__(self, stringIndex):
+
+        super(ConstantStringInfo, self).__init__(CONSTANT_STRING_INFO)
+
+        self.stringIndex = stringIndex
+
+    def getStringIndex(self):
+
+        return self.stringIndex
+
+    def __str__(self):
+
+        return "String: #%d" % self.stringIndex
+
+    @staticmethod
+    def parse(fh):
+
+        assert fh is not None
+
+        return ConstantStringInfo(ByteToDec(fh.read(2)))
+
+class ConstantFieldrefInfo(ConstantInfo):
+
+    def __init__(self, hostClassIndex, nameAndTypeIndex):
+
+        super(ConstantFieldrefInfo, self).__init__(CONSTANT_FIELDREF_INFO)
+
+        self.hostClassIndex = hostClassIndex
+        self.nameAndTypeIndex = nameAndTypeIndex
+
+    def getFieldClassIndex(self):
+
+        return self.hostClassIndex
+
+    def getNameAndTypeIndex(self):
+
+        return self.nameAndTypeIndex
+
+    def __str__(self):
+
+        return 'Fieldref: #%d (#%d)' % (self.hostClassIndex, self.nameAndTypeIndex)
+
+    @staticmethod
+    def parse(fh):
+
+        assert fh is not None
+
+        classInfoIndex = ByteToDec(fh.read(2))
+        nameAndTypeIndex = ByteToDec(fh.read(2))
+
+        return ConstantFieldrefInfo(classInfoIndex, nameAndTypeIndex)
+
+class ConstantMethodrefInfo(ConstantInfo):
+
+    def __init__(self, hostClassIndex, nameAndTypeIndex):
+
+        super(ConstantMethodrefInfo, self).__init__(CONSTANT_METHODREF_INFO)
+
+        self.hostClassIndex = hostClassIndex
+        self.nameAndTypeIndex = nameAndTypeIndex
+
+    def getFieldClassIndex(self):
+
+        return self.hostClassIndex
+
+    def getNameAndTypeIndex(self):
+
+        return self.nameAndTypeIndex
+
+    def __str__(self):
+
+        return 'Methodref: #%d (#%d)' % (self.hostClassIndex, self.nameAndTypeIndex)
+
+    @staticmethod
+    def parse(fh):
+
+        assert fh is not None
+
+        classInfoIndex = ByteToDec(fh.read(2))
+        nameAndTypeIndex = ByteToDec(fh.read(2))
+
+        return ConstantMethodrefInfo(classInfoIndex, nameAndTypeIndex)
+
+class ConstantInterfacerefInfo(ConstantInfo):
+
+    def __init__(self, hostClassIndex, nameAndTypeIndex):
+
+        super(ConstantInterfacerefInfo, self).__init__(CONSTANT_INTERFACEMETHODREF_INFO)
+
+        self.hostClassIndex = hostClassIndex
+        self.nameAndTypeIndex = nameAndTypeIndex
+
+    def getFieldClassIndex(self):
+
+        return self.hostClassIndex
+
+    def getNameAndTypeIndex(self):
+
+        return self.nameAndTypeIndex
+
+    def __str__(self):
+
+        return 'Interfaceref: #%d (#%d)' % (self.hostClassIndex, self.nameAndTypeIndex)
+
+    @staticmethod
+    def parse(fh):
+
+        assert fh is not None
+
+        classInfoIndex = ByteToDec(fh.read(2))
+        nameAndTypeIndex = ByteToDec(fh.read(2))
+
+        return ConstantInterfacerefInfo(classInfoIndex, nameAndTypeIndex)
+
+
+class ConstantNameAndTypeInfo(ConstantInfo):
+
+    def __init__(self, nameIndex, descriptorIndex):
+
+        super(ConstantNameAndTypeInfo, self).__init__(CONSTANT_NAMEANDTYPE_INFO)
+
+        self.nameIndex = nameIndex
+        self.descriptorIndex = descriptorIndex
+
+    def getNameIndex(self):
+
+        return self.nameIndex
+
+    def getDescriptorIndex(self):
+
+        return self.descriptorIndex
+
+    def __str__(self):
+
+        return 'NameAndType: #%d.#%d' % (self.nameIndex, self.descriptorIndex)
+
+    @staticmethod
+    def parse(fh):
+
+        assert fh is not None
+
+        nameIndex = ByteToDec(fh.read(2))
+        descriptorIndex = ByteToDec(fh.read(2))
+
+        return ConstantNameAndTypeInfo(nameIndex, descriptorIndex)
+
+
+
+class ConstantPool:
+    """
+    Utility to parse to constant pool of java class.
+    """
+    @staticmethod
+    def getHandler(type):
+
+        handlers = {
+            CONSTANT_CLASS_INFO: ConstantClassInfo.parse,
+            CONSTANT_UTF8_INFO: ConstantUTF8Info.parse,
+            CONSTANT_INTEGER_INFO: ConstantIntegerInfo.parse,
+            CONSTANT_LONG_INFO: ConstantLongInfo.parse,
+            CONSTANT_FLOAT_INFO: ConstantFloatInfo.parse,
+            CONSTANT_DOUBLE_INFO: ConstantDoubleInfo.parse,
+            CONSTANT_STRING_INFO: ConstantStringInfo.parse,
+            CONSTANT_FIELDREF_INFO: ConstantFieldrefInfo.parse,
+            CONSTANT_METHODREF_INFO: ConstantMethodrefInfo.parse,
+            CONSTANT_INTERFACEMETHODREF_INFO: ConstantInterfacerefInfo.parse,
+            CONSTANT_NAMEANDTYPE_INFO: ConstantNameAndTypeInfo.parse,
+        }
+
+        if type in handlers:
+            return handlers[type]
+        else:
+            return None
+
+    @staticmethod
+    def getContentBytes(type):
+
+        typeContentBytes = {
+            CONSTANT_UTF8_INFO:      2,
+            CONSTANT_INTEGER_INFO:   4,
+            CONSTANT_FLOAT_INFO:     4,
+            CONSTANT_LONG_INFO:      8,
+            CONSTANT_DOUBLE_INFO:    8,
+            CONSTANT_CLASS_INFO:     2,
+            CONSTANT_STRING_INFO:    2,
+            CONSTANT_FIELDREF_INFO:  4,
+            CONSTANT_METHODREF_INFO: 4,
+            CONSTANT_INTERFACEMETHODREF_INFO: 4,
+            CONSTANT_NAMEANDTYPE_INFO: 4
+        }
+
+        if typeContentBytes.has_key(type):
+            return typeContentBytes[type]
+        else:
+            return 0
+
+    @staticmethod
+    def parse(count, fh):
+        constants = []
+
+        skipped = False
+
+        for index in range(count):
+
+            if skipped:
+                skipped = False
+                constants.append(ConstantPlaceHolder())
+                continue
+
+            type = ByteToDec(fh.read(1))
+
+            handler = ConstantPool.getHandler(type)
+            if handler is not None:
+                attribute = handler(fh)
+                constants.append(attribute)
+            else:
+                readBytes = ConstantPool.getContentBytes(type)
+                if readBytes:
+                    fh.read(readBytes)
+                    constants.append(None)
+
+            if type == CONSTANT_LONG_INFO or type == CONSTANT_DOUBLE_INFO:
+                skipped = True
+
+        return constants
+
+
